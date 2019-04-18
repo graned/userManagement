e4e7eacfb130ec82e9af15e86eae1bfe993e0455
Eduardo Anaya <gran_ed@hotmail.com>
2019-04-18 16:43:09 +0200

    change double quotes to single quotes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import UserUseCases from "./domain/useCases/user";
-import UserInteractor from "./domain/interactors/UserInteractor";
-import { UserEntityGateway } from "./infrastructure/UserEntityGateway";
-import { IControllers } from "./presentation/controllers/IControllers";
-import { IUserInBoundary } from "./domain/boundries/IUserInBoundary";
-import { UserControllers } from "./presentation/controllers/userControllers";
+import UserUseCases from './domain/useCases/user';
+import UserInteractor from './domain/interactors/UserInteractor';
+import { UserEntityGateway } from './infrastructure/UserEntityGateway';
+import { IControllers } from './presentation/controllers/IControllers';
+import { IUserInBoundary } from './domain/boundries/IUserInBoundary';
+import { UserControllers } from './presentation/controllers/userControllers';
 
 enum UseCases {
     user,
@@ -30,6 +30,7 @@ class Domain {
         return this.useCases;
     }
 
+    // This are the controllers to be used by the API
     initControllers(useCases: Map<UseCases, IUserInBoundary>): Map<Controllers, IControllers> {
         const userControllers = new UserControllers(useCases.get(UseCases.user));
         this.controllers.set(Controllers.user, userControllers);
