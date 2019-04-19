b407fe22c873d5e2673a1a4465c2462245e57aa1
Eduardo Anaya <gran_ed@hotmail.com>
2019-04-19 22:29:22 +0200

    add id property in response model

--- src/domain/models/UserResponseModel.ts
+++ src/domain/models/UserResponseModel.ts
@@ -2,12 +2,14 @@ import UserEntity from '../entities/UserEntity';
 
 // not to be confused with http requests, this is a class ment to RESPOND the results from domain!
 export class UserResponseModel {
+    id?: number;
     firstName?: string;
     lastName?: string;
     email?: string;
     password?: string;
 
     constructor(userEntity: UserEntity) {
+        this.id = userEntity.id || null;
         this.firstName = userEntity.firstName || null;
         this.lastName = userEntity.lastName || null;
         this.email = userEntity.email || null;
